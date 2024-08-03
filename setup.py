0dbeb8c01b4f1563a8788924f78d2f0c67b589d2
Santiago Minolli <minollisantiago@gmail.com>
2024-08-02 23:26:20 -0300

    include setup.py correctly to allow for package installation

--- setup.py
+++ setup.py
@@ -3,6 +3,7 @@ from setuptools import setup, find_packages
 setup(
     name='hyperpytext',
     version='1.0',
+    description = "A tool to create HyperPy applications",
     packages=find_packages(),
     include_package_data=True,
     package_data={
